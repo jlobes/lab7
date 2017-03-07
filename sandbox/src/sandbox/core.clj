92cbc739733109f1e4f767fbb82f7b4207256d33
Jake <jakeloberti@jakes-mbp-2.wheatonma.edu>
2017-03-07 16:52:55 -0500

    factorial problem!

--- sandbox/src/sandbox/core.clj
+++ sandbox/src/sandbox/core.clj
@@ -1,7 +1,13 @@
 (ns sandbox.core
   (:gen-class))
 
+(defn factorial_comp
+	[n]
+	(reduce * (range 1 (+ n 1)))
+
+	)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (factorial_comp 5)))
